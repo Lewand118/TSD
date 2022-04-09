36c677ac8dfc925f9ffe5311c4ced5324dbfff82
Lewand118 <49614673+Lewand118@users.noreply.github.com>
2022-04-09 20:13:57 +0200

    Submit

---
+++ 04-ASP.NET MVC/Yummy/Yummy/Controllers/RecipesController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.Models;
+
+namespace Yummy.Controllers
+{
+    public class RecipesController : Controller
+    {
+        public static List<Recipes> recipesList = new List<Recipes>();
+        // GET: RecipesController1
+        public ActionResult Index()
+        {
+            return View(recipesList.ToList());
+        }
+
+        // GET: RecipesController1/Details/5
+        public ActionResult Details(int Id)
+        {
+            return View(recipesList.Where(r => r.Id == Id).First());
+        }
+
+        // GET: RecipesController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RecipesController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Recipes recipes)
+        {
+            try
+            {
+                recipesList.Add(recipes);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RecipesController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(recipesList.Where(r => r.Id == id).First());
+        }
+
+        // POST: RecipesController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Recipes recipes)
+        {
+            try
+            {
+                recipesList[id] = recipes;
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RecipesController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(recipesList.Where(r => r.Id == id).First());
+        }
+
+        // POST: RecipesController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                recipesList.Remove(recipesList.Where(r => r.Id == id).First());
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 04-ASP.NET MVC/Yummy/Yummy/Models/Recipes.cs
@@ -0,0 +1,12 @@
+﻿namespace Yummy.Models
+{
+    public class Recipes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+        public string Difficulty { get; set; }
+        public string Ingredients { get; set; }
+        public string Process { get; set; }
+    }
+}
